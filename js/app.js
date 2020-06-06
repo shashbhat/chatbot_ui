aa0c77775f2b97c52fb3c9dba08aaca5c999f945
Shashank <shashank.kakunje@gmail.com>
2020-06-06 21:00:44 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+var outputArea = $("#chat-output");
+
+$("#user-input-form").on("submit", function (e) {
+
+  e.preventDefault();
+
+  var message = $("#user-input").val();
+  botResponse(message);
+  outputArea.append(`
+    <div class='bot-message'>
+      <div class='message'>
+        ${message}
+      </div>
+    </div>
+  `);
+
+  function botResponse(userMsg) {
+
+    // Bot Response
+    $.get( `https://chatbot-v3.herokuapp.com/chat/${userMsg}`, function(data){
+      console.log(userMsg);
+      console.log(data);
+
+      const msgText = data;
+
+      setTimeout(function () {
+        outputArea.append(`
+          <div class='user-message'>
+            <div class='message'>
+              ${msgText}
+            </div>
+          </div>
+        `);
+      }, 250);
+
+      
+    });
+
+  }
+
+  $("#user-input").val("");
+
+});
